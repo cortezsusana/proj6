d2ca949bc60ac06dcada8b1649b801da1ac1a028
cortezsusana <susana.ccortez@gmail.com>
2014-04-28 17:02:39 +0100

    uigig

---
+++ src/java/pt/uc/dei/aor/projeto4/grupog/exceptions/SearchNullException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.uc.dei.aor.projeto4.grupog.exceptions;
+
+/**
+ * @author Elsa Santos
+ * @author Pedro Pamplona
+ */
+public class SearchNullException extends Exception {
+
+    public SearchNullException() {
+        super("Your search returned no results! Try again!");
+    }
+}
---
+++ src/java/pt/uc/dei/aor/projeto4/grupog/managebeans/WebServiceController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pt.uc.dei.aor.projeto4.grupog.managebeans;
+
+import com.chartlyrics.api.GetLyricResult;
+
+/**
+ *
+ * @author Bruno Maricato
+ */
+public class WebServiceController {
+
+    private String result;
+
+    /**
+     * Creates a new instance of WebServiceController
+     */
+    public WebServiceController() {
+    }
+
+    public boolean checkLyric(String artist, String song) {
+        return !getLyricSong(artist, song).isEmpty();
+    }
+
+    public String getLyricSong(String artist, String song) {
+        GetLyricResult glr = searchLyricDirect(artist, song);
+        return this.result = glr.getLyric();
+    }
+
+    private static GetLyricResult searchLyricDirect(java.lang.String artist, java.lang.String song) {
+        com.chartlyrics.api.Apiv1 service = new com.chartlyrics.api.Apiv1();
+        com.chartlyrics.api.Apiv1Soap port = service.getApiv1Soap();
+        return port.searchLyricDirect(artist, song);
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+}
