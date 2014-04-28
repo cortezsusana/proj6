9797bc3d1ef6e95ab0264566b1f10d949edba023
elsasantos <elsateodoros@gmail.com>
2014-04-28 17:26:56 +0100

    webservice

---
+++ src/java/pt/uc/dei/aor/projeto4/grupog/WebService/LyricsWikiRest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.uc.dei.aor.projeto4.grupog.WebService;
+
+/**
+ *
+ * @author Elsa
+ */
+public class LyricsWikiRest {
+
+}
